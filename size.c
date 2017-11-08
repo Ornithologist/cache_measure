b55b96e38054d1226c70160f6d84a070949cb580
Ornithologist <anthing1markmax@gmail.com>
2017-11-07 20:41:33 -0500

    change strategy

--- size.c
+++ size.c
@@ -5,41 +5,44 @@
 #include <time.h>
 #include <unistd.h>
 
-#define MAX_ARRAY_SIZE 100000
-#define MIN_ARRAY_SIZE 3
-#define REPEAT 1000
-#define STRIDE 2
-#define JUMP 10
+#define SIZE_MAGNITUDE 16
+#define MIN_ARRAY_SIZE 1024
+#define REPEAT 1 << 19
+#define STRIDE 32
 #define GNU_CMDS 4
+#define REGURGITATE 80
 
-unsigned long long timer(int n, int s);
+void setup_array(int n, int s, int *array);
+int timer(int n, int s);
 
 int main(int argc, char **argv)
 {
     int one_m_itrs_ms, i, j;
-    unsigned long long time_ms;
+    int time_ms;
     FILE *temp = fopen("size.dat", "w");
     char *gnuplot_cmds[] = {
         "set term png", "set output \"size.png\"",
         "set title \"Time for 1 million iteraion per array size\"",
         "plot 'size.dat'"};
 
+    // warm up
+    for (i = 0; i < (SIZE_MAGNITUDE / 2); i++) {
+        int array_size = MIN_ARRAY_SIZE * (1 << i);
+        time_ms = timer(array_size, STRIDE);
+    }
+
     // calculate
-    for (i = MIN_ARRAY_SIZE; i < MAX_ARRAY_SIZE; i += JUMP) {
-        time_ms = 0;
-        for (j = 0; j < REPEAT; j++) {
-            time_ms += timer(i, STRIDE);
-        }
-        one_m_itrs_ms = (time_ms * 1000 * 1000) / (REPEAT * i);
-        fprintf(temp, "%d %d \n", i, one_m_itrs_ms);
-        // printf("%d: %d ms\n", i, one_m_itrs_ms);
+    for (i = 0; i < SIZE_MAGNITUDE; i++) {
+        int array_size = MIN_ARRAY_SIZE * (1 << i);
+        time_ms = timer(array_size, STRIDE);
+        printf("%d: %d ms\n", array_size, time_ms);
     }
 
     // draw
-    FILE *gnu_pipe = popen("gnuplot -persistent", "w");
-    for (i = 0; i < GNU_CMDS; i++) {
-        fprintf(gnu_pipe, "%s \n", gnuplot_cmds[i]);
-    }
+    // FILE *gnu_pipe = popen("gnuplot -persistent", "w");
+    // for (i = 0; i < GNU_CMDS; i++) {
+    //     fprintf(gnu_pipe, "%s \n", gnuplot_cmds[i]);
+    // }
 
     // finish
     printf("Size of an array element is %d byte, finish plotting.\n",
@@ -48,20 +51,68 @@ int main(int argc, char **argv)
     return 0;
 }
 
-unsigned long long timer(int n, int s)
-{
-    int i, *array = (int *)malloc(sizeof(int) * n);
-
+void setup_array(int n, int s, int *array) {
+    // zero all
+    int i, step, current, next;
     for (i = 0; i < n; i++) {
         array[i] = 0;
     }
 
-    clock_t start = clock(), diff;
+    // golden ratio times the number of items to access, ceil
+    step = (int) ((n / s) * 0.61803398875 + 1);
+    step *= s;
+
+    // generate permutation
+    current = 0;
     for (i = 0; i < n; i += s) {
-        array[i] += 1;
+        next = (current + step) % n;
+        array[current / 4] = next / 4;
+        current = next;
+    }
+
+    // clean up cache
+    for (i = 0; i < n; i++) {
+        array[i] |= 0;
+    }
+}
+
+int timer_inner(int n, int s, int *array) {
+    int i, p, out, rounds = (REPEAT / (n / s)) + 1;
+    i = p = 0;
+
+    clock_t start = clock(), diff;
+    while(1) {
+        p = array[p];
+        p = array[p];
+        p = array[p];
+        p = array[p];
+        // iterated through full permutation
+        if (p == 0) {
+            i += 1;
+            if (i == rounds) {
+                break;
+            }
+        }
     }
     diff = clock() - start;
+    out = (1000 * 1000 * diff) / (rounds * (n / s));
+    
+    return out;
+}
 
-    free(array);
-    return diff;
+int timer(int n, int s)
+{
+    int i, best, cur, *array = (int *) malloc(sizeof(int) * n);
+
+    setup_array(n, s, array);
+
+    for (i = 0; i < REGURGITATE; i++) {
+        cur = timer_inner(n, s, array);
+        if (best == 0 || cur < best) {
+            best = cur;
+        }
+    }
+
+    // free(array);
+    return best;
 }
